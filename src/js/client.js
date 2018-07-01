5d41f12ad644c0883466a8dfa5b71cd57e6b4c43
Alex Marvick <alex@fiduciarytech.com>
2018-07-01 16:19:58 -0700

    Creating switch/case statements for fetching/receiving users

--- src/js/client.js
+++ src/js/client.js
@@ -1,22 +1,54 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
+import axios from 'axios'; // xhr request (XML Http Request)
 import thunk from 'redux-thunk';
 // import logger from 'redux-logger';
 
-const userReducer = function(state={}, action) {
+const initialState = {
+    fetching: false,
+    fetched: false,
+    users: [],
+    error: null,
+}
+
+const userReducer = function(state=initialState, action) {
     switch(action.type) {
-        case "CHANGE_USERNAME": {
-            // state.username = action.payload;
-            state = {...state, username: action.payload};
-            break;
-        }
-        case "CHANGE_NAME": {
-            // state.name = action.payload;
-            state = {...state, name: action.payload};
-            break;
-        }
+        // case "CHANGE_USERNAME": {
+        //     // state.username = action.payload;
+        //     state = {...state, username: action.payload};
+        //     break;
+        // }
+        // case "CHANGE_NAME": {
+        //     // state.name = action.payload;
+        //     state = {...state, name: action.payload};
+        //     break;
+        // }
         // default: {
         //     throw new Error("ERROR!!!! Action does not exist.");
         // }
+
+        case "FETCH_USERS_START" : {
+            return {...state,
+                fetching: true
+            }
+            break;
+        }
+
+        case "FETCH_USERS_ERROR" : {
+            return {...state,
+                fetching: false,
+                error: action.payload
+            }
+            break;
+        }
+
+        case "RECEIVE_USERS": {
+            return {...state,
+                fetching: false,
+                fetched: true,
+                users: action.payload
+            }
+            break;
+        }
     }
     return state
 };
@@ -57,8 +89,17 @@ store.subscribe(() => {
 // store.dispatch({type: "CHANGE_USERNAME", payload: "@amarvick"})
 // store.dispatch({type: "CHANGE_NAME", payload: "Alex"})
 store.dispatch((dispatch) => {
-    dispatch({type: "CHANGE_USERNAME", payload: "@amarvick"})
-    dispatch({type: "CHANGE_NAME", payload: "Alex"})
+    dispatch({type: "FETCH_USERS_START"})
+    axios.get("http://rest.learncode.academy/api/wstern/users")
+        .then((response) => {
+            dispatch({type: "RECEIVE_USERS", payload: response.data})
+        })
+        .catch((err) => {
+            dispatch({type: "FETCH_USERS_ERROR", payload: err})
+        })
+
+    // dispatch({type: "CHANGE_USERNAME", payload: "@amarvick"})
+    // dispatch({type: "CHANGE_NAME", payload: "Alex"})
 })
 
 
